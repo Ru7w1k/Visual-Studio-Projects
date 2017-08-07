4b561675ea329a5058a529adc6d6399b391e52b1
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-07 10:17:59 +0530

    First C# project

---
+++ ConTestApp/ConTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From C#!");
+            Console.Read();
+        }
+    }
+}
