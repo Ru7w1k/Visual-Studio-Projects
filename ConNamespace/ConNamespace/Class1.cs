5244f0e14a43e7d5800d89e2204e57496bf5f87c
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-10 12:40:02 +0530

    c# namespace added

---
+++ ConNamespace/ConNamespace/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConNamespace
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method of the Class1");
+        }
+    }
+
+
+    public static class Base
+    {
+
+    }
+}
---
+++ ConNamespace/ConNamespace/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OtherNamespace;
+using inNS = OtherNamespace.InnerNamespace;
+using GlobalVar;
+
+class Program
+{
+    public static float Main(string[] args)
+    {
+        System.Console.WriteLine("Command Line Arguments size {0}", args.Length);
+
+        for(int i = 0; i < args.Length; i++)
+        {
+            System.Console.WriteLine("args[i] : {0}", args[i]);
+        }
+
+        return 1.0f;
+    }
+}
+
+
+
+namespace ConNamespace
+{
+    /// <summary>
+    ///  XML type comment
+    /// </summary>
+
+
+    class Program
+    {
+        static void Main(int[] args)
+        {
+            System.Console.WriteLine("Test int main");
+        }
+
+        void Test()
+        {
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+
+            inNS.InnerNamespaceClass inc = new inNS.InnerNamespaceClass();
+            System.Console.WriteLine("Other Class from Other Namespace");
+
+            //global::Program.Main();
+            
+        }
+    }
+}
+
+
+namespace OtherNamespace
+{
+    public class OtherClass
+    {
+        public void Print()
+        { 
+            System.Console.WriteLine("Other Class from Other Namespace");
+        }
+    }
+
+    namespace InnerNamespace
+    {
+        public class InnerNamespaceClass
+        {
+            public void Test()
+            {
+                GlobalVar.Console.WriteLine();
+            }
+        }
+    }
+}
+
+namespace GlobalVar
+{
+    public class Console
+    {
+        public static void WriteLine()
+        {
+            System.Console.WriteLine("Test Print");
+        }
+
+    }
+}
