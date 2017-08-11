cb1248160d96fa09c8eb779666da8ae56603754c
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-11 12:24:01 +0530

    added some more basic programs

--- ConSwap/ConSwap/Program.cs
+++ ConSwap/ConSwap/Program.cs
@@ -8,19 +8,26 @@ namespace ConSwap
 {
     class Program
     {
+        char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Two Numbers: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Two Numbers: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
 
             Program p = new Program();
-            p.Swap(ref num1, ref num2);
-            Console.WriteLine(num1 + " " + num2);
+            //p.Swap(ref num1, ref num2);
+            //Console.WriteLine(num1 + " " + num2);
 
-            Console.WriteLine("Enter Number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            p.PrintTable(num);
+            //Console.WriteLine("Enter Number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //p.PrintTable(num);
+
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            str = p.RemoveVowels(str);
+            Console.WriteLine(str);
 
             Console.Read();
         }
@@ -38,6 +45,29 @@ namespace ConSwap
             {
                 Console.WriteLine(num*i);
             }
+        }
+
+        public string RemoveVowels(string str)
+        {
+            StringBuilder strRes = new StringBuilder();
+
+            foreach (char ch in str.Where(a => !vowels.Contains(a)))
+            {
+                Console.Write(ch);
+            }
+
+            Console.WriteLine();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(!vowels.Contains(str[i]))
+                {
+                    strRes.Append(str[i]);
+                    //Console.Write(str[i]);
+                }
+            }                
+
+            return strRes.ToString();
         }
 
     }
