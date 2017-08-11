497bfd7c1e3f7933f0a49ff3fb2d757196543c75
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-11 09:52:25 +0530

    Added Advance Class

---
+++ ConAdvanceClass/ConAdvanceClass/PartialClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAdvanceClass
+{
+    public partial class PartialClass
+    {
+        public void PrintMsg()
+        {
+            Console.WriteLine("Printed From Partial Class");
+            Console.WriteLine("Data Value is: {0}", data);
+        }
+
+    }
+}
---
+++ ConAdvanceClass/ConAdvanceClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAdvanceClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialClass pc = new PartialClass();
+            pc.PrintMsg();
+            pc.PrintMsg("test");
+            pc.Print();
+            Console.Read();
+        }
+    }
+
+    public partial class PartialClass
+    {
+        private int data = 10;
+
+        public void PrintMsg(string data)
+        {
+            Console.WriteLine("This is another method");
+        }
+
+    }
+
+    public partial class PartialClass
+    {
+        public void Print()
+        {
+            Console.WriteLine("This is from third segment");
+        }
+    }
+
+}
