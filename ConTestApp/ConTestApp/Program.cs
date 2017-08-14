feab65b3f427ccba36d713a8c6d3d11d964ece45
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-14 17:25:59 +0530

    assignments 14 08

--- ConTestApp/ConTestApp/Program.cs
+++ ConTestApp/ConTestApp/Program.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConTestApp
 {
+    class Test123 : Form
+    {
+        Button bt = new Button();
+    }
+
     class Program
     {
         public int MyProperty { get; set; }
 
+        public delegate string MyDelegate(string s);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World From C#!");
-            Console.Read();
 
-            List<Student> students = new List<Student>();  
-                       
+            MyDelegate  test = new MyDelegate(Test1);
+            Console.WriteLine("Test1 from delegate: " + test("Test"));
+
+            Console.Read();
+        }
+
+        public static string Test1(string str)
+        {
+            return "Test1";
         }
 
         
