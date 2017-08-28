8924b98ae740df9400b7b26b6866578670c20736
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-28 12:22:33 +0530

    added Windows Service..

---
+++ SerDemoService/SerDemoService/Service1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerDemoService
+{
+    public partial class SerDemoService : ServiceBase
+    {
+        public SerDemoService()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists("SerDemoServiceSource"))
+                System.Diagnostics.EventLog.CreateEventSource("SerDemoServiceSource", "SerDemoServiceLog");
+
+            eventLog1.Source = "SerDemoServiceSource";
+            eventLog1.Log = "SerDemoServiceLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Service SerDemoService started..");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Service SerDemoService stopped..");
+        }
+    }
+}
