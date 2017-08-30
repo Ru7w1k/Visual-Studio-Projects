c7cfe3c1c34c3a72e942a6db4610701abbb44faa
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-30 14:23:46 +0530

    added Security and Permissions

---
+++ WinSecurity/LibFileIO/FileIOHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Permissions;
+
+namespace LibFileIO
+{
+    //[FileIOPermissionAttribute(SecurityAction.Demand, ViewAccessControl = @"C:\", Unrestricted = false)]
+    public class FileIOHelper
+    {        
+        public void WriteToFile(string fileName, string data)
+        {
+            FileIOPermission perm = new FileIOPermission(FileIOPermissionAccess.Read, @"C:\");
+            perm.AddPathList(FileIOPermissionAccess.Read, @"D:\");
+            perm.AddPathList(FileIOPermissionAccess.Write, @"D:\");
+            perm.Demand();
+
+            StreamWriter stream = new StreamWriter(new FileStream(fileName, FileMode.OpenOrCreate));
+            stream.WriteLine(data);
+            stream.Close();
+        }
+
+
+    }
+}
---
+++ WinSecurity/WinSecurity/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibFileIO;
+using System.Windows.Forms;
+
+namespace WinSecurity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd1 = new OpenFileDialog();
+            ofd1.ShowDialog();
+
+            FileIOHelper helper = new FileIOHelper();
+            try
+            {
+                helper.WriteToFile(ofd1.FileName, "This is added line");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
