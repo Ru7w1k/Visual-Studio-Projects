b5f5d581af077ebbc3a71b42691220edda8561ab
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-31 16:44:36 +0530

    updated ASP.NET MVC..

--- ASP.NET MVC/Resources/Strongly_Typed_View/Strongly Typed View/WebApplication1/Controllers/HomeController.cs
+++ ASP.NET MVC/Resources/Strongly_Typed_View/Strongly Typed View/WebApplication1/Controllers/HomeController.cs
@@ -3,33 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebFirstMVC.Models;
 
-namespace WebFirstMVC.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult EmpView()
-        {
-            Employee emp = new Employee();
-            emp.EmpId = 101;
-            emp.Name = "Employee";
-            emp.Salary = 50000;
-            return View(emp);
-        }
-
-
-        [ActionName("CurrentTime")]
-        public ActionResult GetCurrentTime()
-        {
-            ViewBag.Time = DateTime.Now.ToLongTimeString();
-
-            return View("CurrentTime");
         }
 
         public ActionResult About()
