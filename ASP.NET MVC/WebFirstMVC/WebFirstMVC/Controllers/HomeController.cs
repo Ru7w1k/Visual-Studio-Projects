fc1b6cab5cd44c5c6ef583730ed36bb26288b04f
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2017-08-31 11:46:12 +0530

    ASP.NET MVC

---
+++ ASP.NET MVC/WebFirstMVC/WebFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("CurrentTime")]
+        public ActionResult GetCurrentTime()
+        {
+
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+
+            return View("CurrentTime");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC/WebFirstMVC/WebFirstMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFirstMVC.MvcApplication" Language="C#" %>
