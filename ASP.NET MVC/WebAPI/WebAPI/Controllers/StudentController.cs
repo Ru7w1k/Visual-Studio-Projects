68374b6315a1e83fc58d63ca2b639a37ba762d98
Rutwik Choughule <Rutwik.Choughule@Xoriant.com>
2017-09-06 16:34:39 +0530

    updated WebAPI..

---
+++ ASP.NET MVC/WebAPI/WebAPI/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public static List<Student> _students = new List<Student>()
+        {
+            new Student() { RollNo = 1, Name = "One" },
+            new Student() { RollNo = 2, Name = "Two" },
+            new Student() { RollNo = 3, Name = "Three" }
+        };
+
+        // GET: Student
+        public IEnumerable<Student> Get()
+        {
+            return _students;
+        }
+
+        public object Get(int id)
+        {
+            IEnumerable<Student> student = _students.Where(s => s.RollNo == id);
+            if (student.Count() > 0)
+                return student.First();
+            else
+                return new { Error = "No Student Found"};
+        }
+
+        // POST: Student
+        public IEnumerable<Student> Post([FromBody] Student data)
+        {
+            _students.Add(data);
+            return _students;
+        }
+
+        public object Put(int id, [FromBody] Student data)
+        {
+            IEnumerable<Student> student = _students.Where(s => s.RollNo == id);
+            if (student.Count() > 0)
+            {
+                student.First().Name = data.Name;
+                return _students;
+            }
+            else
+            {
+                return new { Error = "No Student Found" };
+            }            
+        }
+
+        public object Delete(int id)
+        {
+            IEnumerable<Student> student = _students.Where(s => s.RollNo == id);
+            if (student.Count() > 0)
+            {
+                _students.Remove(student.First());
+                return _students;
+            }
+            else
+            {
+                return new {Error = "No Student Found"};
+            }
+        }
+
+
+    }
+}
