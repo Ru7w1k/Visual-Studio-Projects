effc65f116db2fbefb43c4689cb5334206a99332
Rutwik Choughule <Rutwik.Choughule@Xoriant.com>
2017-09-18 16:29:53 +0530

    added ConSQL..

---
+++ ConSQL/ConSQL/SQLUtil.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConSQL
+{
+    public static class SQLUtil
+    {
+        [SqlFunction]
+        public static string AppendHello(string message)
+        {
+            return message + " Hello";
+        }
+    }
+}
