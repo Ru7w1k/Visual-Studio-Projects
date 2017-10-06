c72db22fdf2f8da50d96200daf7186882ea21d12
Rutwik Choughule <Rutwik.Choughule@Xoriant.com>
2017-10-06 14:07:57 +0530

    updated knockout project, integrated knockout and mvc

--- KnockOut/KOFirst/KOFirst/default.js
+++ KnockOut/KOFirst/KOFirst/default.js
@@ -13,43 +13,106 @@ $(document).ready(function () {
         }
     };
 
-    function Sample(name, marks) {
+    //function Sample(name, marks) {
+    //    var self = this;
+
+    //    self.name = ko.observable(name);
+    //    self.marks = ko.observable(marks);
+    //    self.grade = ko.computed(function () {
+    //        if (self.marks() > 75) {
+    //            return "A";
+    //        }
+    //        else if (self.marks() > 40) {
+    //            return "B";
+    //        }
+    //        else {
+    //            return "C";
+    //        }
+    //    });
+    //};
+
+    //function SampleViewModel() {
+    //    var self = this;
+
+    //    self.list = ko.observableArray([
+    //        new Sample("Test", 100),
+    //        new Sample("ABC", 50),
+    //        new Sample("XYZ", 20)
+    //    ]);
+
+    //    self.setEditMode = ko.observable(false);
+    //    self.setSample = ko.observable(new Sample("",0));
+
+    //    self.removeSample = function (sample) {
+    //        self.list.remove(sample);
+    //    };
+
+    //    self.addSample = function () {
+    //        var name = $('#name').val();
+    //        var marks = $('#marks').val();
+
+    //        if (name === undefined || name === "")
+    //            return;
+
+    //        if (isNaN(marks) || marks < 0 || marks > 100)
+    //            return;
+
+    //        self.list.push(new Sample(name, marks));
+    //    };
+
+    //    self.editSample = function (sample) {
+    //        self.setSample(new Sample(sample.name(), sample.marks()));
+    //        self.setEditMode(true);
+    //    };
+
+    //    self.saveSample = function () {
+    //        for (var i = 0 ; i < self.list().length ; i++) {
+    //            if (self.list()[i].name() == self.setSample().name()) {
+    //                self.list()[i].marks(self.setSample().marks());
+    //                console.log("Value updated! " + self.setSample().marks() + " " + self.list()[i].marks());
+    //                self.setEditMode(false);
+    //                break;
+    //            }
+    //        }
+    //    };
+    //}
+
+    function Student(rollno, name, marks) {
         var self = this;
 
+        self.rollno = ko.observable(rollno);
         self.name = ko.observable(name);
         self.marks = ko.observable(marks);
-        self.grade = ko.computed(function () {
-            if (self.marks() > 75) {
-                return "A";
-            }
-            else if (self.marks() > 40) {
-                return "B";
-            }
-            else {
-                return "C";
-            }
-        });
     };
 
-    function SampleViewModel() {
+    function StudentViewModel() {
         var self = this;
 
-        self.list = ko.observableArray([
-            new Sample("Test", 100),
-            new Sample("ABC", 50),
-            new Sample("XYZ", 20)
-        ]);
-
+        self.list = ko.observableArray([]);
         self.setEditMode = ko.observable(false);
-        self.setSample = ko.observable(new Sample("",0));
+        self.setStudent = ko.observable(new Student(0,"", 0));
 
-        self.removeSample = function (sample) {
-            self.list.remove(sample);
+        self.clearList = function () {
+            self.list([]);
+        }
+
+        self.getStudents = function () {
+            $.get("http://localhost:52255/api/students",
+                null,
+                function (students) {
+                    for (var i = 0 ; i < students.length ; i++) {
+                        self.list.push(new Student(students[i].RollNo, students[i].Name, students[i].Marks));
+                    }
+                }, "JSON");
         };
 
-        self.addSample = function () {
+        self.addStudent = function () {
+            var rollno = $('#rollno').val();
             var name = $('#name').val();
             var marks = $('#marks').val();
+
+            if (rollno === undefined || isNaN(rollno))
+                return;
 
             if (name === undefined || name === "")
                 return;
@@ -57,31 +120,65 @@ $(document).ready(function () {
             if (isNaN(marks) || marks < 0 || marks > 100)
                 return;
 
-            self.list.push(new Sample(name, marks));
-        };
-
-        self.editSample = function (sample) {
-            console.log("inside edit sample", self.setEditMode());
-            self.setSample(new Sample(sample.name(), sample.marks()));
-            self.setEditMode(true);
-            console.log("after setting sample", self.setEditMode());
-        };
-
-        self.saveSample = function () {
-            console.log("Inside SaveSample");
-            for (var i = 0 ; i < self.list().length ; i++) {
-                if (self.list()[i].name() == self.setSample().name()) {
-                    self.list()[i].marks(self.setSample().marks());
-                    console.log("Value updated! " + self.setSample().marks() + " " + self.list()[i].marks());
-                    self.setEditMode(false);
-                    break;
+            $.ajax({
+                url: "http://localhost:52255/api/students/",
+                type: 'POST',
+                data: { RollNo: $("#rollno").val(), Name: $("#name").val(), Marks: $("#marks").val() },
+                success: function (result) {
+                    alert("Success");
+                    self.clearList();
+                    self.getStudents();
+                },
+                error: function (result) {
+                    console.log(result);
+                    alert(result);
                 }
-            }
+            });            
         };
 
+        self.editStudent = function (student) {
+            self.setStudent(new Student(student.rollno(), student.name(), student.marks()));
+            self.setEditMode(true);
+        };
+
+        self.removeStudent = function (student) {
+            $.ajax({
+                url: "http://localhost:52255/api/students/" + student.rollno(),
+                type: 'DELETE',
+                success: function (data) {
+                    alert(data);
+                    self.clearList();
+                    self.getStudents();
+                },
+                error: function (data) {
+                    alert(data);
+                }
+            });
+        };
+
+        self.saveStudent = function () {
+            $.ajax({
+                url: "http://localhost:52255/api/students/" + self.setStudent().rollno(),
+                type: 'PUT',
+                data: { Name: self.setStudent().name(), Marks: self.setStudent().marks() },
+                success: function (result) {
+                    alert(result);
+                    self.setEditMode(false);
+                    self.clearList();
+                    self.getStudents();
+                },
+                error: function (result) {
+                    console.log(result);
+                    alert(result);
+                }
+            });
+            
+        };
     }
 
-    ko.applyBindings(new SampleViewModel());
+    var vm = new StudentViewModel()
+    vm.getStudents();
+    ko.applyBindings(vm);
 
 
 });
---
+++ MVCBasicCRUD/StudentAPI/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVCBasicCRUD.Models;
+using System.Web.Http.Results;
+
+namespace StudentAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public JsonResult<List<Student>> Get()
+        {
+            return Json(StudentContext.Students());
+        }
+
+        public Student Get(int id)
+        {
+            return StudentContext.StudentByRollNo(id);
+        }
+
+        public bool Post([FromBody]Student student)
+        {
+            return StudentContext.InsertStudent(student);
+        }
+
+        public bool Delete(int id)
+        {
+            return StudentContext.DeleteStudent(id);
+        }
+
+        public bool Put(int id, [FromBody]Student student)
+        {
+            student.RollNo = id;
+            return StudentContext.UpdateStudent(student);
+        }
+
+    }
+}
